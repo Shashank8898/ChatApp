49dbfc3e866b614ddb38cfddb64ca0467c1ea5de
Shashank <tiwarishashank950@gmail.com>
2025-10-09 20:49:40 +0530

    Setting up backend

---
+++ backend/src/index.js
@@ -0,0 +1,20 @@
+dotenv.config();
+
+import express from 'express';
+import dotenv from "dotenv";
+
+import {connectDB} from "../lib/db.js"
+//Importing Routes
+import authRoutes from "../routes/auth.route.js"
+//Importing Enviroment variables
+const PORT = process.env.PORT;
+
+const app = express()
+
+//Authorization Route
+app.use("/api/auth",authRoutes)
+
+app.listen(PORT,()=>{
+    console.log(`Backend is online on port ${PORT}`)
+    connectDB;
+}) 
