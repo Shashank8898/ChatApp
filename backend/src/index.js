42479dedf3aac35fda48f2977ae949e305ae1f73
Shashank <tiwarishashank950@gmail.com>
2025-10-10 11:48:37 +0530

    Setting up the message routes

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,6 +7,7 @@ import cookieParser from "cookie-parser"
 import {connectDB} from "../lib/db.js"
 //Importing Routes
 import authRoutes from "../routes/auth.route.js"
+import messageRoutes from "../routes/message.route.js"
 //Importing Enviroment variables
 const PORT = process.env.PORT;
 
@@ -17,6 +18,7 @@ app.use(cookieParser());                            //To allow retrieval of jwt 
 
 //Authorization Route
 app.use("/api/auth",authRoutes)
+app.use("/api/message",messageRoutes)
 
 app.listen(PORT,()=>{
     console.log(`Backend is online on port ${PORT}`)
