e1d3625b783f2eb5e096e48f5ea1d4ef8a0115d0
Shashank <tiwarishashank950@gmail.com>
2025-10-11 12:13:43 +0530

    Finishing the ChatApp

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,33 +1,42 @@
-dotenv.config();
-
-import express from 'express';
+import express from "express";
 import dotenv from "dotenv";
-import cookieParser from "cookie-parser"
+import cookieParser from "cookie-parser";
 import cors from "cors";
 
+import path from "path";
 
-import {connectDB} from "../lib/db.js"
-//Importing Routes
-import authRoutes from "../routes/auth.route.js"
-import messageRoutes from "../routes/message.route.js"
-//Importing Enviroment variables
+import { connectDB } from "./lib/db.js";
+
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import { app, server } from "./lib/socket.js";
+
+dotenv.config();
+
 const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
-const app = express()
+app.use(express.json({ limit: '50mb' }));
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
-app.use(express.json())                             //A middleware that allows us to extract data from req.body
-app.use(cookieParser());                            //To allow retrieval of jwt tokens from cookiess
-app.use(cors({
-    origin:"http://localhost:5173",
-    credentials:true
-}))
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-//Authorization Route
-app.use("/api/auth",authRoutes)
-app.use("/api/message",messageRoutes)
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
-app.listen(PORT,()=>{
-    console.log(`Backend is online on port ${PORT}`)
-    connectDB();
-}) 
+server.listen(PORT, () => {
+  console.log("server is running on PORT:" + PORT);
+  connectDB();
+});
